1154bd9a1eac7954bd1549f170000cd07b3c7759
preshu1 <shresthapreshu26@gmail.com>
2025-11-07 23:21:42 +0545

    refined using nested function

--- tempConverter.js
+++ tempConverter.js
@@ -1,11 +1,11 @@
-function toFahrenheit(temp) {
+/*function toFahrenheit(temp) {
   let result = temp * (9 / 5) + 32;
   console.log(`${temp}C is ${result}F`);
   return result;
 }
 
 function toCelsius(temp) {
-  let result = ((temp - 32) * 5) / 9;
+  let result = (temp - 32) * 5 / 9;
   console.log(`${temp}F is ${result}C`);
   return result;
 }
@@ -25,4 +25,29 @@ function tempConvert(temp, msg) {
   return { original: temp, converted: result };
 }
 tempConvert(25, "to fahrenheit");
-tempConvert(77, "to Celsius");
+tempConvert(77, "to Celsius");*/
+
+//using nested function
+
+function tempConvert(temp, msg) {
+  //inner functions
+  let result;
+  function toC() {
+    return ((temp - 32) * 5) / 9;
+  }
+
+  function toF() {
+    return temp * (9 / 5) + 32;
+  }
+
+  if (msg === "to celsius") {
+    result = toC();
+  } else if (msg === "to fahrenheit") {
+    result = toF();
+  } else {
+    console.log("Please choose one 'to Fahrenheit' or 'to Celcius' to proceed!");
+    return;
+  }
+  return result;
+}
+tempConvert(25, "to fahrenheit");
